02edebcd7ea6cf76e1f6fb0f11580d2656359321
Eissanoor <eissaanoor@gmail.com>
2025-09-28 06:50:59 -0700

    Implement authentication middleware for manage location routes to secure create, update, and delete operations

--- routes/manageLocationRoutes.js
+++ routes/manageLocationRoutes.js
@@ -8,6 +8,7 @@ const {
   deleteManageLocation,
   getManageLocationsByLocationType
 } = require('../controllers/manageLocationController');
+const authMiddleware = require('../middleware/auth');
 
 // GET /api/manage-locations - Get all manage locations
 router.get('/', getAllManageLocations);
@@ -15,14 +16,14 @@ router.get('/', getAllManageLocations);
 // GET /api/manage-locations/:id - Get manage location by ID
 router.get('/:id', getManageLocationById);
 
-// POST /api/manage-locations - Create new manage location
-router.post('/', createManageLocation);
+// POST /api/manage-locations - Create new manage location (protected)
+router.post('/', authMiddleware.verifyToken, createManageLocation);
 
-// PUT /api/manage-locations/:id - Update manage location
-router.put('/:id', updateManageLocation);
+// PUT /api/manage-locations/:id - Update manage location (protected)
+router.put('/:id', authMiddleware.verifyToken, updateManageLocation);
 
-// DELETE /api/manage-locations/:id - Delete manage location
-router.delete('/:id', deleteManageLocation);
+// DELETE /api/manage-locations/:id - Delete manage location (protected)
+router.delete('/:id', authMiddleware.verifyToken, deleteManageLocation);
 
 // GET /api/manage-locations/location-type/:locationTypeId - Get manage locations by location type
 router.get('/location-type/:locationTypeId', getManageLocationsByLocationType);
